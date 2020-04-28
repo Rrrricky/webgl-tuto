faf446b490862b4fe9ff14dddb27b8f00f20641e
Rrrricky <eric.dufreche@hetic.net>
2020-04-29 01:56:42 +0200

    :tada: Git init

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import './styles/main.scss'
 import houseGroup from './house/mesh'
+import particles from './particles/mesh'
 import objectsGroup, { sphere, plane, torusKnot } from './basics/mesh'
 import { pointLight, pointLightHelper } from './basics/lights'
 import camera from './house/camera'
@@ -11,12 +12,14 @@ import { mouseMove, resizeWindow } from './house/controls'
 const scene = new THREE.Scene()
 scene.add(houseGroup)
 scene.add(objectsGroup)
+scene.add(particles)
 scene.add(camera)
 scene.add(pointLight) // Optional (for some materials)
 scene.add(pointLightHelper)
 renderer.render(scene, camera)
 
 houseGroup.visible = false
+objectsGroup.visible = false
 
 
 mouseMove()
@@ -29,5 +32,7 @@ const loop = () => {
   sphere.rotation.y += 0.002
   plane.rotation.y += 0.002
   torusKnot.rotation.y += 0.002
+  particles.rotation.y += 0.002
+  particles.rotation.y += 0.002
 }
 loop()
